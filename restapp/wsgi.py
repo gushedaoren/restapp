fab34243cbdc532c66beaed2e10900185ff7503d
olvier <gushedaoren@gmail.com>
2015-09-02 16:38:50 +0800

    sys path

--- restapp/wsgi.py
+++ restapp/wsgi.py
@@ -10,6 +10,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+
+path = '/var/restapp/'
+
+if path not in sys.path:
+
+    sys.path.insert(0, path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restapp.settings")
 
