7a40d2f87f0f195a52eaac03b1424f496bd5d54f
oliver <oliver@nixuchen.com>
2015-09-04 20:31:29 -0700

    add home page

--- restapp/urls.py
+++ restapp/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url,patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-
-
-
+from chongming import views
 
 urlpatterns =patterns('',
                url(r'^admin/', include(admin.site.urls)),
 
                url(r'^', include('oliver.urls')),
                url(r'^', include('chongming.urls')),
+               url(r'$', views.index),
                       )
 urlpatterns += staticfiles_urlpatterns()
