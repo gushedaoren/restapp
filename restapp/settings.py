7ceddef4e067024c349316e75fcd27bf86711080
olvier <gushedaoren@gmail.com>
2015-09-10 15:40:47 +0800

    static root

--- restapp/settings.py
+++ restapp/settings.py
@@ -130,7 +130,7 @@ DEFAULT_CHARSET = 'utf-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'staticfiles/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
