d1f4ba3310cdcbd588480cea792f4a4c485e0ef3
olvier <gushedaoren@gmail.com>
2015-09-11 15:43:21 +0800

    add migrations

---
+++ chongming/migrations/0005_auto_20150911_1534.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('chongming', '0004_auto_20150911_1248'),
+    ]
+
+    operations = [
+    ]
